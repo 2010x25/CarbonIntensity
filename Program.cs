bf1f7562bc4d5e705c83007794f78abda3b5cc46
Kishan Suryawanshi <kishan.suryawanshi@gmail.com>
2021-02-10 01:14:56 +0800

    Added code to start the pulling logic infinetely in Main

--- Program.cs
+++ Program.cs
@@ -8,7 +8,26 @@ namespace CarbonIntensity
     {
         static async Task Main(string[] args)
         {
-            
+            var dataProvider = new ApiProvider();
+            var dataStorage = new SqlServerStorage(Constants.DbConnectionString);
+            var processor = new Processor(dataProvider, dataStorage);
+
+            while (true)
+            {
+                Console.WriteLine("----------- Fetching API service -------");
+                try
+                {
+                    processor.Process();
+                }
+                catch (Exception)
+                {
+                    break;  // Assume momentary outrage of API or DB, try again in next interval as specified in Constants
+                }
+                
+                Console.WriteLine("---------Going into Sleep model-----------");
+
+                await Task.Delay(Constants.IntervalInMinutes * 60 * 1000);
+            }
         }
     }
 }
